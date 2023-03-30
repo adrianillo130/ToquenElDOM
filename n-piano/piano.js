f2e659cc6972bbb6cf50630bdaafb30c9ba273f4
GeorgeGuRu <georgegutierrezruiz@gmail.com>
2023-03-30 14:04:32 +0200

    he actualizado el audio del piano a medias

--- n-piano/piano.js
+++ n-piano/piano.js
@@ -1,6 +1,6 @@
-const teclasIds = ['tecla1m', 'tecla2m', 'tecla3m', 'tecla4m', 'tecla5m', 'tecla6m', 'tecla7m', 'tecla8m', 'tecla9m', 'tecla10m', 'tecla11m'];
+const teclasIds = ['teclado1', 'teclado#1', 'teclare1', 'teclare#1', 'teclami1', 'teclafa1', 'teclafa#1', 'teclasol1', 'teclasol#1', 'teclala1', 'teclala#1', 'teclasi1', 'teclado2', 'teclado#2', 'teclare2', 'teclare#2', 'teclami2', 'teclafa2', 'teclafa#2', 'teclasol2', 'teclasol#2', 'teclala2', 'teclala#2', 'teclasi2', 'teclado3', 'teclado#3', 'teclare3', 'teclare#3', 'teclami3', 'teclafa3', 'teclafa#3', 'teclasol3', 'teclasol#3', 'teclala3', 'teclala#3', 'teclasi'3];
 
-const archivoSonido = ['sonidos/11 FA2.mp3', 'sonidos/10 MI2.mp3', 'sonidos/9 RE2.mp3', 'sonidos/8 DO_01.mp3', 'sonidos/7 SI.mp3', 'sonidos/6 LA.mp3', 'sonidos/5 SOL.mp3', 'sonidos/4 FA.mp3', 'sonidos/3 MI.mp3', 'sonidos/2 RE.mp3', 'sonidos/1-DO-Tecla+grande.mp3'];
+const archivoSonido = ['sonidos-piano/1-do1.mp3', 'sonidos/10 MI2.mp3', 'sonidos/9 RE2.mp3', 'sonidos/8 DO_01.mp3', 'sonidos/7 SI.mp3', 'sonidos/6 LA.mp3', 'sonidos/5 SOL.mp3', 'sonidos/4 FA.mp3', 'sonidos/3 MI.mp3', 'sonidos/2 RE.mp3', 'sonidos/1-DO-Tecla+grande.mp3'];
 
 for (let i = 0; i < teclasIds.length; i++) {
 let tecla = document.getElementById(teclasIds[i]);
