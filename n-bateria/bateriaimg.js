8ff034a6665c3f8c3271e0b8bf1db936944f893a
GeorgeGuRu <georgegutierrezruiz@gmail.com>
2023-04-03 10:58:50 +0200

    bateria en proceso

---
+++ n-bateria/bateriaimg.js
@@ -0,0 +1,5 @@
+const teclasIds = ['crash1img', 'splashimg', 'crash2img', 'hi-hatimg', 'tom1img', 'tom2img', 'snareimg', 'bassimg', 'kickimg'];
+
+const teclasTap = ['crash1-tap', 'splash-tap', 'crash2-tap', 'hi-hat-tap', 'tom1-tap', 'tom2-tap', 'snare-tap', 'bass-tap', 'kick-tap'];
+
+/* const archivoSonido = ['sonidos/11 FA2.mp3', 'sonidos/10 MI2.mp3', 'sonidos/9 RE2.mp3', 'sonidos/8 DO_01.mp3', 'sonidos/7 SI.mp3', 'sonidos/6 LA.mp3', 'sonidos/5 SOL.mp3', 'sonidos/4 FA.mp3', 'sonidos/3 MI.mp3', 'sonidos/2 RE.mp3', 'sonidos/1-DO-Tecla+grande.mp3']; */
