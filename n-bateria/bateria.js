2994a230db3a585005c3e6155b6ec2bf40b0b0ed
GeorgeGuRu <georgegutierrezruiz@gmail.com>
2023-04-04 13:03:49 +0200

    he añadido audio a bateria no-funcional y medio-arreglado el color de las teclas del piano

--- n-bateria/bateria.js
+++ n-bateria/bateria.js
@@ -1,1 +1,28 @@
-const teclasIds = ['tecla1m', 'tecla2m', 'tecla3m', 'tecla4m', 'tecla5m', 'tecla6m', 'tecla7m', 'tecla8m', 'tecla9m', 'tecla10m', 'tecla11m'];
+const teclasIds = ['crash1img', 'splashimg', 'crash2img', 'hi-hatimg', 'tom1img', 'tom2img', 'snareimg', 'bassimg', 'kickimg'];
+
+const teclasTap = ['crash1-tap', 'splash-tap', 'crash2-tap', 'hi-hat-tap', 'tom1-tap', 'tom2-tap', 'snare-tap', 'bass-tap', 'kick-tap'];
+
+const archivoSonido = ['sonidos/Crash 01.mp3', 'sonidos/Splash 01.mp3', 'sonidos/Crash 02.mp3', 'sonidos/hi-hat.mp3', 'sonidos/tom1.mp3', 'sonidos/tom2.mp3', 'sonidos/snare.mp3', 'sonidos/Bass drum hit.mp3', 'sonidos/kick.wav'];
+
+/* --------------------------------------------------- */
+
+function cambioColorTeclas(teclasIds, teclasTap) {
+    for (let i = 0; i < teclas.length; i++) {
+    const tecla = teclas[i];
+    document.addEventListener('keydown', (e) => {
+        if (e.key === tecla.letra) {
+        let teclaBlanca = document.getElementById(teclasIds[i])
+        teclaBlanca.style.backgroundColor = "#C48F8F"
+        console.log(teclaBlanca);
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        if (e.key === tecla.letra) {
+        let teclaBlanca = document.getElementById(teclasIds[i])
+        teclaBlanca.style.backgroundColor = "rgb(255, 255, 255)"
+        console.log(teclaBlanca);
+        }
+    });
+    }
+}
+cambioColorTeclas(teclas, teclasIds);
--- n-piano/piano.js
+++ n-piano/piano.js
@@ -11,7 +11,7 @@ function sonidoMouse(teclasIds, archivoSonido) {
         etiquetaAudio.play();
     });
     }
-    let boton = document.querySelector(".reproductor");
+    /* let boton = document.querySelector(".reproductor"); */
 }
 sonidoMouse(teclasIds, archivoSonido);
 
@@ -70,7 +70,7 @@ function sonidoKeyboard(teclas) {
 }
 sonidoKeyboard(teclas);
 
-function cambioColorTeclas(teclas, teclasIds) {
+/* function cambioColorTeclas(teclas, teclasIds) {
     for (let i = 0; i < teclas.length; i++) {
     const tecla = teclas[i];
     document.addEventListener('keydown', (e) => {
@@ -89,4 +89,89 @@ function cambioColorTeclas(teclas, teclasIds) {
     });
     }
 }
-cambioColorTeclas(teclas, teclasIds);
+cambioColorTeclas(teclas, teclasIds); */
+
+
+const teclasB = [
+    { letra: "q", sonido: "sonidos-piano/1-do1.mp3" },
+    { letra: "w", sonido: "sonidos-piano/3-re1.mp3" },
+    { letra: "e", sonido: "sonidos-piano/5-mi1.mp3" },
+    { letra: "r", sonido: "sonidos-piano/6-fa1.mp3" },
+    { letra: "t", sonido: "sonidos-piano/8-sol1.mp3" },
+    { letra: "y", sonido: "sonidos-piano/10-la1.mp3" },
+    { letra: "u", sonido: "sonidos-piano/12-si1.mp3" },
+    { letra: "z", sonido: "sonidos-piano/13-do2.mp3" },
+    { letra: "x", sonido: "sonidos-piano/15-re2.mp3" },
+    { letra: "c", sonido: "sonidos-piano/17-mi2.mp3" },
+    { letra: "v", sonido: "sonidos-piano/18-fa2.mp3" },
+    { letra: "b", sonido: "sonidos-piano/20-sol2.mp3" },
+    { letra: "n", sonido: "sonidos-piano/22-la2.mp3" },
+    { letra: "m", sonido: "sonidos-piano/24-si2.mp3" },
+    { letra: "Z", sonido: "sonidos-piano/25-do3.mp3" },
+    { letra: "X", sonido: "sonidos-piano/27-re3.mp3" },
+    { letra: "C", sonido: "sonidos-piano/29-mi3.mp3" },
+    { letra: "V", sonido: "sonidos-piano/30-fa3.mp3" },
+    { letra: "B", sonido: "sonidos-piano/32-sol3.mp3" },
+    { letra: "N", sonido: "sonidos-piano/34-la3.mp3" },
+    { letra: "M", sonido: "sonidos-piano/36-si3.mp3" },
+];
+
+const teclasN = [
+    { letra: "2", sonido: "sonidos-piano/2-do#1.mp3" },
+    { letra: "3", sonido: "sonidos-piano/4-re#1.mp3" },
+    { letra: "5", sonido: "sonidos-piano/7-fa#1.mp3" },
+    { letra: "6", sonido: "sonidos-piano/9-sol#1.mp3" },
+    { letra: "7", sonido: "sonidos-piano/11-la#1.mp3" },
+    { letra: "s", sonido: "sonidos-piano/14-do#2.mp3" },
+    { letra: "d", sonido: "sonidos-piano/16-re#2.mp3" },
+    { letra: "g", sonido: "sonidos-piano/19-fa#2.mp3" },
+    { letra: "h", sonido: "sonidos-piano/21-sol#2.mp3" },
+    { letra: "j", sonido: "sonidos-piano/23-la#2.mp3" },
+    { letra: "S", sonido: "sonidos-piano/26-do#3.mp3" },
+    { letra: "D", sonido: "sonidos-piano/28-re#3.mp3" },
+    { letra: "G", sonido: "sonidos-piano/31-fa#3.mp3" },
+    { letra: "H", sonido: "sonidos-piano/33-sol#3.mp3" },
+    { letra: "J", sonido: "sonidos-piano/35-la#3.mp3" },
+];
+
+function cambioColorTeclasB(teclasB, teclasIds) {
+    for (let i = 0; i < teclasB.length; i++) {
+    const teclab = teclasB[i];
+    document.addEventListener('keydown', (e) => {
+        if (e.key === teclab.letra) {
+        let teclaBlanca = document.getElementById(teclasIds[i])
+        teclaBlanca.style.backgroundColor = "#C48F8F"
+        console.log(teclaBlanca);
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        if (e.key === teclab.letra) {
+        let teclaBlanca = document.getElementById(teclasIds[i])
+        teclaBlanca.style.backgroundColor = "rgb(255, 255, 255)"
+        console.log(teclaBlanca);
+        }
+    });
+    }
+}
+cambioColorTeclasB(teclasB, teclasIds);
+
+function cambioColorTeclasN(teclasN, teclasIds) {
+    for (let i = 0; i < teclasN.length; i++) {
+    const teclan = teclasN[i];
+    document.addEventListener('keydown', (e) => {
+        if (e.key === teclan.letra) {
+        let teclaNegra = document.getElementById(teclasIds[i])
+        teclaNegra.style.backgroundColor = "#C48F8F"
+        console.log(teclaNegra);
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        if (e.key === teclan.letra) {
+        let teclaNegra = document.getElementById(teclasIds[i])
+        teclaNegra.style.backgroundColor = "#000000"
+        console.log(teclaNegra);
+        }
+    });
+    }
+}
+cambioColorTeclasB(teclasN, teclasIds);
