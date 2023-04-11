0b239de6158dd621c574ac23cdd746ed120fe4cc
Adrián <supadri2002@gmail.com>
2023-04-11 14:01:15 +0200

    He añadido un const al javascript de bateria llamado instrumentopartesbateria y dentro está las teclas del teclado con las rutas de los sonidos. Por último pues quedaría arreglar lo de los sonidos con el mouse y el teclado y lo del color al darle.

--- n-bateria/bateria.js
+++ n-bateria/bateria.js
@@ -6,23 +6,39 @@ const archivoSonido = ['sonidos/Crash 01.mp3', 'sonidos/Splash 01.mp3', 'sonidos
 
 /* --------------------------------------------------- */
 
-function cambioColorTeclas(partesbateriaIds, partesbateriaTap) {
+function cambioColorBateria(partesbateriaIds, partesbateriaTap) {
     for (let i = 0; i < partesbateriaTap.length; i++) {
-    const parte = partesbateriaTap[i];
-    document.addEventListener('keydown', (e) => {
-        if (e.key === parte.letra) {
-        let bateria = document.getElementById(partesbateriaIds[i])
-        bateria.style.backgroundColor = "#C0C0C0"
-        console.log(bateria);
-        }
-    })
-    document.addEventListener('keyup', (e) => {
-        if (e.key === parte.letra) {
-        let bateria = document.getElementById(partesbateriaIds[i])
-        bateria.style.backgroundColor = "#C48F8F"
-        console.log(bateria);
-        }
-    });
+        const parte = partesbateriaTap[i];
+        document.addEventListener('keydown', (e) => {
+            if (e.key === parte.letra) {
+                let bateria = document.getElementById(partesbateriaIds[i])
+                bateria.style.backgroundColor = "#C0C0C0"
+                console.log(bateria);
+            }
+        })
+        document.addEventListener('keyup', (e) => {
+            if (e.key === parte.letra) {
+                let bateria = document.getElementById(partesbateriaIds[i])
+                bateria.style.backgroundColor = "#C48F8F"
+                console.log(bateria);
+            }
+        });
     }
 }
-cambioColorTeclas(partesbateriaTap, partesbateriaIds);
+cambioColorBateria(partesbateriaTap, partesbateriaIds);
+
+
+
+
+
+const instrumentopartesbateria = [
+    { letra: "q", sonido: "/n-bateria/sonidos/Bass drum hit.mp3" },
+    { letra: "2", sonido: "/n-bateria/sonidos/Crash 01.mp3" },
+    { letra: "w", sonido: "/n-bateria/sonidos/Crash 02.mp3" },
+    { letra: "3", sonido: "/n-bateria/sonidos/hi-hat.mp3" },
+    { letra: "e", sonido: "/n-bateria/sonidos/kick.wav" },
+    { letra: "r", sonido: "/n-bateria/sonidos/snare.mp3" },
+    { letra: "5", sonido: "/n-bateria/sonidos/Splash 01.mp3" },
+    { letra: "t", sonido: "/n-bateria/sonidos/tom1.mp3" },
+    { letra: "6", sonido: "/n-bateria/sonidos/tom2.mp3" },
+];
